f1068cdb983806cb3ea0b7fa85823b32a8931d4f
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2021-04-05 20:10:21 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const users = [{
+    name: 'Fernando S. Maransatto',
+    age: 32,
+    weight: 113.2,
+    gender: 'male',
+    city: 'Pompéia',
+    state: 'SP'
+},{
+    name: 'José da Silva',
+    age: 40,
+    weight: 77.1,
+    gender: 'male',
+    city: 'Brasilia',
+    state: 'DF'
+},{
+    name: 'Dona Maria',
+    age: 39,
+    weight: 70,
+    gender: 'female',
+    city: 'São Paulo',
+    state: 'SP'
+},{
+    name: 'Miguel Silva',
+    age: 9,
+    weight: 41.6,
+    gender: 'male',
+    city: 'Rio e Janeiro',
+    state: 'RJ'
+},{
+    name: 'Ana Paula',
+    age: 32,
+    weight: 69.6,
+    gender: 'female',
+    city: 'Bauru',
+    state: 'SP'
+}];
+
+
+// --- FILTER ---
+// Pega todos os usuários com o peso acima de 75 kg.
+// Pega todos com a idade menor que 35 anos
+// Pega todos os masculinos
+// Pega todos os que moram no estado de São Paulo
+
+
+//--- MAP ---
+// Traz um array com todos os nomes
+// Traz um array com todas as idades
+// Traz um array de objeto com apenas o nome e o gênero
+// Traz ums array com o primeiro nome de todos
+
+
+// --- REDUCE ---
+// Pega a soma do peso de todos
+// Pega a soma de idade de todos
+// Pega o número total de caracteres no nome de todos os usuários
+// Pega o número de caracteres pelo gênero
+
+
+// --- SORT ---
+// Ordena por nome
+// Ordena pelo peso
+// Ordena pela idade
+// Ordena pelo gênero
+
+
+// --- EVERY ---
+// Todos os usuários são de SP?
+// Todos os usuários tem mais que 100 kg?
+// Todos os usuários tem menos de 90 anos?
+// todos os usuários são masculinos?
+
+
+// --- SOME ---
+// Tem pelo menos um usuário de SP?
+// Tem pelo menos um usuário acima de 100 kg?
+// Tem pelo menos um usuário com menos de 90 anos?
+// Tem pelo menos um usuário masculino?
